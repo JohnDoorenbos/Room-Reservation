fdb9df6df9003c7c2d188e85b0be9006ff6c0773
mertzy <mertis01@luther.edu>
2014-03-30 15:48:34 -0500

    changes for inputs

--- errorCheck.js
+++ errorCheck.js
@@ -1,25 +1,100 @@
 function checkForInputErrors() { //this function will check to see if the selection corresponds with the input the user types in
 
-    var error = "this is an error"
-
-/*
-
-    selectorData = document.getElementById("selector")
-    userInputDate = document.getElementById("userInput")
+    var error = ""
 
 
-    if 
+    var selectorData = document.getElementById("selector")
+    var userInputData = document.getElementById("userInput")
 
-*/
 
-    return error
+    if (errorCheckForDate()==true && selectorData=="forDate") { 
+
+	error = "Input error. The date is not of the correct format."
+	
+	return error
+
+    }
+
+    else if (errorCheckForRoom()==true && selectorData=="forRoom") {
+
+	error = "Input error. The room is not of the correct format."
+	
+	return error
+
+    }
+
+    else {
+
+	error = ""
+
+	return error
+
+    }
+
+
+}
+
+function searchArray(arr, obj) { //this function returns true if obj is in arr
+
+    for(var i=0; i<arr.length; i++) {
+
+	if (arr[i] == obj) {
+
+	    return true;
+
+	}
+
+    }
+
+function errorCheckForDate() {
+
+    //this function will check to see if the date entered follows the correct format
+
+    var userInputData = document.getElementById("userInput")
+
+    if (!(searchArray(dateList, userInputData))) { //need to create date list
+
+	return true;
+
+    }
+
+    else {
+
+	return false;
+
+    }
+
+    
 
 }
 
 
+function errorCheckForRoom() {
+
+    //this function will check to see if the date entered follows the correct format
+
+    var userInputData = document.getElementById("userInput")
+
+    if (!(searchArray(roomList, userInputData))) {
+
+	return true;
+
+    }
+
+    else {
+
+	return false;
+
+    }
+
+	
+
+}
+
 
 function printErrors() { //actually prints the error
 
-    document.getElementById("output").innterHTML = ""
-    document.getElementById("output").innerHTML = checkForInputErrors()
+    var document.getElementById("output").innterHTML = ""
+    var document.getElementById("output").innerHTML = checkForInputErrors()
+
 }
