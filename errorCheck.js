b6d979cbb68d242611d30529559bdf4db0f55bd3
mertzy <mertis01@luther.edu>
2014-03-30 14:48:28 -0500

    additions for styling and buttons

---
+++ errorCheck.js
@@ -0,0 +1,25 @@
+function checkForInputErrors() { //this function will check to see if the selection corresponds with the input the user types in
+
+    var error = "this is an error"
+
+/*
+
+    selectorData = document.getElementById("selector")
+    userInputDate = document.getElementById("userInput")
+
+
+    if 
+
+*/
+
+    return error
+
+}
+
+
+
+function printErrors() { //actually prints the error
+
+    document.getElementById("output").innterHTML = ""
+    document.getElementById("output").innerHTML = checkForInputErrors()
+}
